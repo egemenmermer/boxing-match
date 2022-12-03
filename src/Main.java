cbc03174f398bb2dbb7d3dff9c91b4bd72a489bb
Egemen Mermer <egemenmermer@gmail.com>
2022-12-03 17:05:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Fighter f1 = new Fighter("Marc" , 15 , 100, 90, 0);
+        Fighter f2 = new Fighter("Alex" , 10 , 95, 100, 0);
+        Match r = new Match(f1,f2 , 90 , 100);
+        r.run();
+    }
+}
